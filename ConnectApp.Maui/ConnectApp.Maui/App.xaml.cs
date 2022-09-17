a202f8c746339c4ad4be24181c49e906e9a1cd28
Lewis Westbury <lewis@cantab.net>
2022-09-17 19:13:07 +0100

    Add a blank MAUI app

---
+++ ConnectApp.Maui/ConnectApp.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ConnectApp.Maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ ConnectApp.Maui/ConnectApp.Maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ConnectApp.Maui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
