f13367d222b4a155f6a49910182db536fce4dbc9
Lewis Westbury <lewis@cantab.net>
2023-05-07 02:31:42 +0100

    Rebuild for .NET 7 begins - added firebase plugin, compilation and code signing succesful

--- ConnectApp.Maui/ConnectApp.Maui/MauiProgram.cs
+++ ConnectApp.Maui/ConnectApp.Maui/MauiProgram.cs
@@ -1,4 +1,12 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+using Plugin.Firebase.Auth;
+
+#if IOS
+using Plugin.Firebase.Core.Platforms.iOS;
+#else
+using Plugin.Firebase.Core.Platforms.Android;
+#endif
 
 namespace ConnectApp.Maui;
 
@@ -9,17 +17,40 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+			.RegisterFonts()
+            .RegisterFirebaseServices();
 
 #if DEBUG
 		builder.Logging.AddDebug();
 #endif
 
 		return builder.Build();
-	}
+    }
+
+    private static MauiAppBuilder RegisterFirebaseServices(this MauiAppBuilder builder)
+    {
+        builder.ConfigureLifecycleEvents(events => {
+#if IOS
+            events.AddiOS(iOS => iOS.FinishedLaunching((_,__) => {
+                CrossFirebase.Initialize();
+                return false;
+            }));
+#else
+            events.AddAndroid(android => android.OnCreate((activity, _) =>
+                CrossFirebase.Initialize(activity)));
+#endif
+        });
+
+        builder.Services.AddSingleton(_ => CrossFirebaseAuth.Current);
+        return builder;
+    }
+
+    private static MauiAppBuilder RegisterFonts(this MauiAppBuilder builder) {
+        return builder.ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+        });
+    }
 }
 
